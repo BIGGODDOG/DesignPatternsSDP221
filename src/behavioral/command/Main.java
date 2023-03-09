b2cfb8bdaf04ed074cbe0c9a92f94d9d73d8c2b6
BIGGODDOG <Gann.ia98@gmail.com>
2023-03-09 17:02:13 +0600

    Chain Of Responsibility added

---
+++ src/behavioral/command/Main.java
@@ -0,0 +1,12 @@
+package behavioral.command;
+
+import behavioral.chainofresponsibility.AbstractLogger;
+import behavioral.chainofresponsibility.ConsoleLogger;
+import behavioral.chainofresponsibility.ErrorLogger;
+import behavioral.chainofresponsibility.FileLogger;
+
+public class Main {
+    public static void main(String[] args){
+        Stock stock = new Stock();
+    }
+}
